b621fbdc09fce481237eea30195e6faaa13e0c07
anwaraldleme33-a11y <anwaraldleme33@gmail.com>
2026-02-02 09:28:04 +0300

    Add files via upload

---
+++ api/add.js
@@ -0,0 +1,16 @@
+import { neon } from "@neondatabase/serverless";
+const sql = neon(process.env.DATABASE_URL);
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST')
+    return res.status(405).json({ message:'Method not allowed' });
+
+  const { device_id,temperture,humidity,pressure,windS,windD} = req.body;
+
+  await sql`
+    INSERT INTO informations(device_id,p_name,ph_no,email,age,checkin_date)
+    VALUES(${device_id},${temperture},${humidity},${pressure},${windS},${windD})
+  `;
+
+  res.json({ success:true });
+}
